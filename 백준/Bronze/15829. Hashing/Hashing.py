7814a4ecbce18fb38130ed0c4d4aab045939985c
YooJin Lee <113789141+youznn@users.noreply.github.com>
2023-01-23 20:21:55 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,11 +1,11 @@
-from math import pow
 from sys import stdin
 n = stdin.readline()
 arr = stdin.readline()
 sum = 0
 j = 0
-for i in arr[:-1]:
-    sum += (ord(i)-96) * pow(31,j)
+for i in range(len(arr)-1):
+    sum += (ord(arr[i])-96) * 31 ** i
     j += 1
 
+sum %= 1234567891
 print(int(sum))
