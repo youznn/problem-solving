ab0b8935274a439dcc98b45fcb02bcaece051335
YooJin Lee <113789141+youznn@users.noreply.github.com>
2023-12-19 23:42:49 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -4,16 +4,24 @@ com_num = int(input())
 link_num = int(input())
 
 dic = {}
-for i in range (link_num):
-    a,b = map(int,sys.stdin.readline().split())
-    dic[a] = dic.get(a,[]) + [b]
-    dic[b] = dic.get(b,[]) + [a]
+for i in range(link_num):
+    a, b = map(int, sys.stdin.readline().split())
+    dic[a] = dic.get(a, []) + [b]
+    dic[b] = dic.get(b, []) + [a]
 
-def dfs(dic, v, visited= []):
-    visited.append(v)
-    for node in dic[v]:
+
+def dfs(s, dict):
+    stack_dfs= []
+    visited = set()
+    start_node = s
+    stack_dfs.append(start_node)
+    if s not in dic.keys():
+        return [s]
+    while stack_dfs:
+        node = stack_dfs.pop()
         if node not in visited:
-            dfs(dic , node , visited)
+            visited.add(node)
+            stack_dfs.extend(dict[node])
     return visited
 
-print(len(dfs(dic,1))-1)
+print(len(dfs(1,dic))-1)
