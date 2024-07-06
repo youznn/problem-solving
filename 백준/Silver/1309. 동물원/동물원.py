1e7c84399229acc4a7202f6391e5a9999a78e645
YooJin Lee <113789141+youznn@users.noreply.github.com>
2024-07-06 23:19:44 +0900

    [Silver I] Title: 동물원, Time: 976 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -5,10 +5,10 @@
 # 바로 위 오른쪽에 사자가 있으면 해당 라인에는 왼쪽에 사자 가능 (경우의 수 2)
 # 바로 위 왼쪽에 사자가 있으면 해당 라인에는 오른쪽 사자 가능 (경우의 수 2)
 # 아무것도 배치하지 않는 경우는 모두 가능
-
+import sys
 case = [1 for _ in range (3)]
 
-N = int(input())
+N = int(sys.stdin.readline())
 
 for i in range (N-1):
     temp = sum(case)
